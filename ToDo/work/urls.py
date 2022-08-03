89e9eed481303cce12b20741e110ea61ca15f266
shri142 <shrikrishna2002umbare@gmail.com>
2022-08-04 00:03:25 +0530

    update urls.py

--- ToDo/work/urls.py
+++ ToDo/work/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 
 urlpatterns = [
-    path('login/', views.login,name='kt'),
+    path('login/', views.login,name='login'),
     path('', views.index,name='index'),
-    path("logout/", views.logout, name="homepage"),
+    path("logout/", views.logout, name="logout"),
 
 
 ]
--- ToDo/work/views.py
+++ ToDo/work/views.py
@@ -4,13 +4,13 @@ from django.shortcuts import render , HttpResponse , redirect
 # Create your views here.
 
 def index(request):
-    render(request, 'index.html')
+    return render(request, 'index.html')
 
 def login(request):
-    render(request, 'login.html')
+    return render(request, 'login.html')
 
 def logout(request):
-    render(request, 'logout.html')
+    return render(request, 'login.html')
 
 
 
